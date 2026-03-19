8ddcf4d270560e6061c7b1eeacf12def31556886
martinadams.dev <martinadams.dev@gmail.com>
2026-03-19 23:45:21 +0800

    🔥 remove legacy Astro files from web/ (keep Templ templates + static assets)

--- web/src/components/layout/index.ts
+++
@@ -1,7 +0,0 @@
-export { Sidebar } from './Sidebar';
-export { Header } from './Header';
-export { ThemeToggle } from './ThemeToggle';
-export { LocaleSwitcher } from './LocaleSwitcher';
-export { DashboardShell } from './DashboardShell';
-export { adminNavSections, allNavItems } from './nav-items';
-export type { NavItem, NavSection } from './nav-items';
